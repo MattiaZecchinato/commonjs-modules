3dea47c7cf11269157c846037c8b95bf50278798
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-24 14:44:52 +0100

    feat: add people script

---
+++ people.js
@@ -0,0 +1,31 @@
+import funcNames from './names.js';
+import funcHobbies from './hobbies.js';
+
+const retObj = retNamesHobbies();
+console.log(retObj);
+
+/******* 
+function
+*******/
+
+function retNamesHobbies () {
+
+    const objNames = funcNames('mattia', 'zecchinato'); 
+
+    const fullName = `${objNames.firstName} ${objNames.lastName}`;
+
+    const arrayHobbies = funcHobbies('f1', 'pallanuoto', 'leggere');
+
+    const objNamesHobbies = {
+
+        fullName,
+        hobbies: {
+
+            hobbiesOne: arrayHobbies[0],
+            hobbiesTwo: arrayHobbies[1],
+            hobbiesThree: arrayHobbies[2]
+        }
+    };
+
+    return objNamesHobbies;
+}
