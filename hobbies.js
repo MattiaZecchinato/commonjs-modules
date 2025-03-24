8d79e27c4f0bf416213af42c3b0e56e3e2c8db65
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-24 14:44:44 +0100

    feat: add hobbies script

---
+++ hobbies.js
@@ -0,0 +1,13 @@
+function retHobbies (hobbyOne, hobbyTwo, hobbyThree) {
+
+    
+    const arrayHobbies = [
+        hobbyOne.toString(), 
+        hobbyTwo.toString(), 
+        hobbyThree.toString()
+    ];
+
+    return arrayHobbies;
+}
+
+export default retHobbies;
